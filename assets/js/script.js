31b8545323da6ccacbd04d41e75043180be1fafd
JosephJoeyJoe <88215210+JosephJoeyJoe@users.noreply.github.com>
2021-11-19 12:45:11 -0500

    created variables and window prompts

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,39 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// function to generate password
+
+function generatePassword() {
+    var lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "f", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    var uppercaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    var specialCharacters = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "-", "=", "{", "[", "}", "]", ":", ";", "'", "<", ",", ">", ".", "/", "?", "|"];
+    var selections = []; 
+
+ getSelections();
+
+ function getSelections() {
+     getLowercase = confirm ("Do you want your password to include lowercase letters?");
+     if (getLowercase) {
+         selections = selections.concat(lowercaseLetters);
+     }
+ }
+
+    getUppercase = confirm ("Do you want your password to include uppercase letters?");
+    if (getUppercase) {
+        selections = selections.concat(uppercaseLetters);
+    }
+
+
+    getNumbers = confirm ("Do you want your password to include numbers?");
+    if (getNumbers) {
+        selections = selections.concat(numericCharacters);
+    }
+
+    getSpecialCharacters = confirm(specialCharacters);
+    if (getSpecialCharacters) {
+        selections = selections.concat(specialCharacters);
+    }
+
+};
